28ce31643a4440e2f06280e4740c1a133509a372
NaufalSatriaa <satriana0017@gmail.com>
2023-04-07 09:24:44 +0700

    looping untuk menentukan panjang array

--- easd_1_175_D/easd_1_175_D.cpp
+++ easd_1_175_D/easd_1_175_D.cpp
@@ -9,5 +9,14 @@ int mov_count = 0;
 int n;
 
 void input() {
-	
+	while (true) {
+		cout << "masukkan panjang element array" << endl;
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nmaksimum panjang array adalah 20" << endl;
+	}
+
 }
